c4b07c49d846cc6b12d5a6367f34832924939d7b
Oleg Aushev <aushevom@gmail.com>
2023-06-02 12:40:59 +0300

    add drivers

--- drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_utils.c
+++ drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_utils.c
@@ -21,7 +21,7 @@
 #include "stm32f4xx_ll_system.h"
 #include "stm32f4xx_ll_pwr.h"
 #ifdef  USE_FULL_ASSERT
-#include "stm32_assert.h"
+// OBSOLETE #include "stm32_assert.h"
 #else
 #define assert_param(expr) ((void)0U)
 #endif /* USE_FULL_ASSERT */
